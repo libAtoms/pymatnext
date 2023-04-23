60fab47f7a97b21f5ba4c5d42da436fef9439703
Noam Bernstein <noam.bernstein@nrl.navy.mil>
2023-04-23 11:37:04 -0400

    Add initial_config_file parameter to [ns] section

--- pymatnext/ns_params.py
+++ pymatnext/ns_params.py
@@ -7,5 +7,6 @@ param_defaults = {
     "n_walkers": ["_REQ_", 1],
     "walk_length": ["_REQ_", 1],
     "configs_module": ["_REQ_", "ns_config_mod"],
-    "exit_conditions": { "_IGNORE_": True }
+    "exit_conditions": { "_IGNORE_": True },
+    "initial_config_file": "_NONE_"
 }
