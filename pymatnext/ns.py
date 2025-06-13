04d914a4fb9de9d25a1bbeac3d41b0e81e847d90
Noam Bernstein <noam.bernstein@nrl.navy.mil>
2025-06-13 16:59:46 -0400

    Add timeout to mpi restart test, and temporarily revert fix in pymatnext/ns.py so that timeout actually occurs

--- pymatnext/ns.py
+++ pymatnext/ns.py
@@ -209,7 +209,7 @@ class NS:
                     raise RuntimeError(f"Got too many configs (expected {self.n_configs_global}) from new config generator {new_configs_generator}")
 
                 # Check that all step sizes are the same. Maybe instead we should just copy from first?
-                assert new_config.step_size == first_config.step_size, f"Mismatched step size for config {config_i} {new_config.step_size} != 0 {first_config.step_size}"
+                #NB assert new_config.step_size == first_config.step_size, f"Mismatched step size for config {config_i} {new_config.step_size} != 0 {first_config.step_size}" #NB
 
                 target_rank = config_i // self.max_n_configs_local
                 if target_rank == self.comm.rank:
@@ -358,6 +358,13 @@ class NS:
                     print("step_size_tune initial", name, "size", size, "max", max_size, "freq", freq)
                 first_iter = False
 
+            # It looks like the following should always give the same values, hence exit
+            # condition, on all MPI tasks, but this is not guaranteed and can lead to deadlocks
+            # in the allreduce. The reason is that the value of done_i in the loop depends
+            # on the value returned from _tune_from_accept_rate, which depends on the previous
+            # step size, and if those are inconsistent between MPI tasks (as in
+            # https://github.com/libAtoms/pymatnext/issues/20), a deadlock may occur.
+            # Only fix is to make sure this doesn't happen (https://github.com/libAtoms/pymatnext/pull/23)
             done = []
             for param_i in range(n_params):
                 if accept_freq[param_i][0] > 0:
@@ -376,8 +383,8 @@ class NS:
             for ns_config in self.local_configs:
                 ns_config.step_size = new_step_size
             # make sure that config used as buffer also has correct step_size
-            if self.extra_config:
-                self.extra_config.step_size = new_step_size
+            #NB if self.extra_config: #NB
+                #NB self.extra_config.step_size = new_step_size #NB
 
             # if self.comm.rank == 0:
                 # print("step_size_tune done", list(zip(done, accept_freq, step_size)))
