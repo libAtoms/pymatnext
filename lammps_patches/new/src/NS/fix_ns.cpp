2c831775416cf2cc2c0ebff4eb5b5ec6c5648f38
Noam Bernstein <noam.bernstein@nrl.navy.mil>
2026-08-31 10:00:45 -0400

    Make debug default false in lammps fix ns, lammps dump traj to gzip file, and fix typo in report interval if

--- lammps_patches/new/src/NS/fix_ns.cpp
+++ lammps_patches/new/src/NS/fix_ns.cpp
@@ -174,9 +174,9 @@ FixNS::FixNS(LAMMPS *lmp, int narg, char **arg) :
     if (strcmp(arg[iarg++], "debug") != 0)
         error->all(FLERR, "Unknown argument when searching for 'debug'");
 
-    debug = 1;
-    if (strcmp(arg[iarg],"no") == 0) debug = 0;
-    else if (strcmp(arg[iarg],"yes") != 0)
+    debug = 0;
+    if (strcmp(arg[iarg],"yes") == 0) debug = 1;
+    else if (strcmp(arg[iarg],"no") != 0)
       error->all(FLERR,"Illegal fix ns/type debug value");
     iarg++;
   }
