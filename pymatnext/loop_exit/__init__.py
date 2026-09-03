da142b33a0a6ca77f7d308c821ea24869d9c404b
Noam Bernstein <noam.bernstein@nrl.navy.mil>
2026-09-03 17:26:49 -0400

    Use dataclass params in loop_exit

--- pymatnext/loop_exit/__init__.py
+++ pymatnext/loop_exit/__init__.py
@@ -14,11 +14,11 @@ class NSLoopExit():
     """
 
     def __init__(self, params, ns):
-        params = NSLoopExitParams.model_validate(params).model_dump()
+        params = NSLoopExitParams.model_validate(params)
 
-        exit_module = params["module"]
+        exit_module = params.module
         if exit_module is not None and exit_module != "_NONE_":
-            self.exit_evaluator = importlib.import_module(exit_module).ExitLoop(ns=ns, **params["module_kwargs"])
+            self.exit_evaluator = importlib.import_module(exit_module).ExitLoop(ns=ns, **params.module_kwargs)
         else:
             self.exit_evaluator = lambda loop_iter, max_val: False
 
